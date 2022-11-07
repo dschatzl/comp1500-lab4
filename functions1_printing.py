e82375d173fe765a32078ff28e382c14e4138a91
dschatzl <derek@schatzlein.com>
2022-11-07 11:03:47 -0600

    Add files via upload

---
+++ functions1_printing.py
@@ -0,0 +1,13 @@
+# add your function definitions BELOW this line
+
+
+
+
+
+
+
+# add your function definitions ABOVE this line
+
+if __name__ == '__main__':
+    print_hello_world()
+    # insert calls to your functions here to test them out
